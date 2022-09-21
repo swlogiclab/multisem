d5930e25f6f400feb4930a53c6d3d175a096bfd3
Colin S. Gordon <colin.s.gordon@gmail.com>
2022-09-20 23:18:28 -0400

    worked out more efficient specialization of Jacobson binding

--- Multisem/Grammar.lean
+++ Multisem/Grammar.lean
@@ -4,21 +4,52 @@ import Multisem.Text.Macros
 
 universe u v t
 
+/--
+The enumeration of supported prepositional phrase varieties for English
+-/
 inductive PPType : Type := | IN | INTO | TO | FROM | OF | OFN
 deriving instance Repr for PPType
 
--- We're going to go full traditional CCG here
 inductive Cat.{q} : Type (q+1)  :=
 | S : Cat
 | NP : forall {x:Type q}, Cat
 | ADJ : forall {x:Type q}, Cat
 | CN : forall {x:Type q}, Cat
 | PP : forall {x:Type q}, PPType -> Cat
+| Ref : Cat -> Cat -> Cat
 | rslash : Cat  -> Cat  -> Cat 
 | lslash : Cat  -> Cat  -> Cat 
 open Cat
 
--- This deriving breaks if any of the category instances are explicit arguments
+namespace Notations
+  -- These are some currently-disabled notations for writing the slashes
+  -- At the moment we can't get lexicon entries working with a mix of explicit and notation-based categories, which we need for backwards compat right now
+  -- We can allow writing right slashes by implementing Div for Cat
+  instance CatDiv.{q} : Div (Cat.{q}) where
+    div := rslash
+
+  -- This is probably not best practice, but we really do need typeclass resolution to unfold these right now. Later if we are more uniform in using the notations for lexicon entries, we could probably remove these attribute overrides.
+  attribute [simp] HDiv.hDiv
+  attribute [simp] Div.div
+
+  theorem _checkCatDiv : (S / (@NP Nat)) = rslash S (@NP Nat) := by simp
+  -- We also want to write left slashes
+  class LDiv.{q} (α : Type q) where
+    lDiv : α → α → α
+  attribute [simp] LDiv.lDiv
+
+  -- CRITICAL: This is not simply backslash, but \setminus! Lean reserves the backslash character because it's used by all Lean editors for unicode lead character
+  infixr:70 " ∖ " => LDiv.lDiv
+  macro_rules |  `($x ∖ $y) => `(binop% LDiv.lDiv $x $y)
+
+  instance CatLDiv.{q} : LDiv (Cat.{q}) where
+    lDiv := lslash
+  theorem _checkCatLDiv : ((@NP Nat) ∖ S) = lslash (@NP Nat) S := by rfl
+end Notations
+instance CatMod.{q} : Mod (Cat.{q}) where
+  mod := Ref
+
+-- This deriving breaks if any of the category instances are explicit arguments, because in general it cannot print types
 deriving instance Repr for Cat
 #eval reprPrec (rslash (lslash S S) S) 234
 #eval (rslash (lslash S S) S)
@@ -41,6 +72,7 @@ def interp (P:Type u) (c:Cat) : Type u :=
   | @ADJ x => x -> P
   | rslash a b => interp P b -> interp P a
   | lslash a b => interp P a -> interp P b
+  | Ref a b => interp P b -> interp P a
   | @CN x => x -> P
   -- The variety of prepositional phrase has not semantic content, they're basically syntactic tags for d"isambiguation
   | @PP x PPType.OFN => x -> P -- This is a bit of a hack to make stuff like "of naturals" work, but I haven't found a clear discussion of "of CN" in the literature yet
@@ -101,6 +133,8 @@ attribute [simp] Synth.denotation
 instance (P:Type u)(ws:tree String) (c:Cat) : Repr (Synth P ws c) where
   reprPrec inst n := inst.stringRep
 
+
+
 instance SynthLex (P:Type u){w:String}{C:Cat}[l:lexicon P w C] : Synth P (tree.one w) C where
   denotation := lexicon.denotation w
   stringRep := "lexicon<"++w++":"++ (reprPrec C 0) ++">"
@@ -130,6 +164,11 @@ instance LComp (P:Type u){s s' c1 c2 c3}[L:Synth P s (lslash c1 c2)][R:Synth P s
   denotation x := R.denotation _ (L.denotation _ x)
   stringRep := "(LComp "++L.stringRep++" "++R.stringRep++")"
 
+-- Lifting rules for exponents a la Jacobson/Morrill/Carpenter
+-- This is used for pronouns and currently for definite articles
+-- TODO: lift referents over slash types. Follow Jaeger's exposition.
+
+
 -- TODO: Need to add type raising!
 
 -- English-specific lifting rules
@@ -138,6 +177,99 @@ instance MLift (H:Type u){T U:Type u}{s}[sem:Synth H s (rslash (lslash (@NP T) S
   Synth H s (rslash (lslash (@NP T) S) (rslash S (lslash (@NP U) S))) where 
   stringRep := "(MLift "++sem.stringRep++")"
   denotation := fun P x => P (fun y => sem.denotation _ y x)
+
+/-
+  The rules in here make the search space *much* larger, so they are disabled
+  by default.
+-/
+namespace Anaphora
+  -- Slightly simplified (concretized) Jacobson-style extraction (e.g., for anaphora), per Jaeger (p100)
+  -- Jacobson restricts the automatic raising to cases where the extraction is a NP, which is all we need now, and also prevents these from completely trashing performance with unconstrained search
+  local instance (priority := low) GR {P:Type u}[HeytingAlgebra P]{X A B}{C:Type u}[base:Synth P X (rslash A B)]
+    : Synth P X (rslash (A % (@NP C)) (B % (@NP C))) where
+    denotation := fun x y => base.denotation _ (x y)
+    stringRep := "(G> "++base.stringRep++")"
+  local instance (priority := low) GL {P:Type u}[HeytingAlgebra P]{X A B}{C:Type u}[base:Synth P X (lslash B A)]
+    : Synth P X (lslash (B % (@NP C)) (A % (@NP C))) where
+    denotation := fun x y => base.denotation _ (x y)
+    stringRep := "(G< "++base.stringRep++")"
+  
+  local instance (priority := low) ZRR {P:Type u}[HeytingAlgebra P]{X A B C}[base:Synth P X (rslash (rslash A (@NP B)) C)]
+    : Synth P X (rslash (rslash A (@NP B)) (C % (@NP B))) where
+    denotation := fun x y => base.denotation _ (x y) y
+    stringRep := "(Z>> "++base.stringRep++")"
+  local instance (priority := low) ZLR {P:Type u}[HeytingAlgebra P]{X A B C}[base:Synth P X (rslash (lslash (@NP B) A) C)]
+    : Synth P X (rslash (lslash (@NP B) A) (C % (@NP B))) where
+    denotation := fun x y => base.denotation _ (x y) y
+    stringRep := "(Z<> "++base.stringRep++")"
+  local instance (priority := low) ZRL {P:Type u}[HeytingAlgebra P]{X A B C}
+    [base:Synth P X (lslash C (rslash A (@NP B)))]
+    : Synth P X (lslash (C % (@NP B)) (rslash A (@NP B))) where
+    denotation := fun x y => base.denotation _ (x y) y
+    stringRep := "(Z>< "++base.stringRep++")"
+  local instance (priority := low) ZLL {P:Type u}[HeytingAlgebra P]{X A B C}
+    [base:Synth P X (lslash C (lslash (@NP B) A))]
+    : Synth P X (lslash (C % (@NP B)) (lslash (@NP B) A)) where
+    denotation := fun x y => base.denotation _ (x y) y
+    stringRep := "(Z<< "++base.stringRep++")"
+
+  /- Even with the NP restrictions, the above still explode performance because
+     they essentially result in TC resolution lifting random transitive verbs and such.
+     As an alternative, let's consider what the Z rules actually do:
+     they lift function types so they can combine with arguments that have extracted referents. But they allow *unnecessary* lifting.
+     What if instead we fused them with application, yielding specialized application rules that only apply when it's necessary to combine with an extraction? That should control the slow-down, but would be subtle.
+
+     These rules are much less general than the arbitrary lifting, because they won't automatically play well with other combinators --- we may end up needing quite a few more of these. Each seems to slow down search a little bit, so we will keep these in their own module: these will be scoped instances. We'll keep the original rules around as local instances (which will never be considered in real searches) so we can prove conservativity
+  -/
+  /-- A condensation of `GR` and `SynthRApp` -/
+  scoped instance AppGR {P:Type u}[HeytingAlgebra P]{X Y A B C}
+    [f:Synth P X (rslash A B)][arg:Synth P Y (B % (@NP C))]
+    : Synth P (X # Y) (A % (@NP C)) where
+    denotation := fun c => f.denotation _ (arg.denotation _ c)
+    stringRep := "(AppGR "++f.stringRep++" "++arg.stringRep++")"
+  /-- A condensation of `GL` and `SynthLApp` -/
+  scoped instance AppGL {P:Type u}[HeytingAlgebra P]{X Y A B C}
+    [arg:Synth P X (B % (@NP C))][f:Synth P Y (lslash B A)]
+    : Synth P (X # Y) (A % (@NP C)) where
+    denotation := fun c => f.denotation _ (arg.denotation _ c)
+    stringRep := "(AppGL "++f.stringRep++" "++arg.stringRep++")"
+  scoped instance AppZRR {P:Type u}[HeytingAlgebra P]{X Y A B C}
+    [f:Synth P X (rslash (rslash A (@NP B)) C)][arg:Synth P Y (C % (@NP B))]
+    : Synth P (X # Y) (rslash A (@NP B)) where
+    denotation := fun n => f.denotation _ (arg.denotation _ n) n
+    stringRep := "(AppZRR "++f.stringRep++" "++arg.stringRep++")"
+  theorem AppZRR_conservative {P X Y A B C}[HeytingAlgebra P]
+    (f:Synth P X (rslash (rslash A (@NP B)) C))(arg:Synth P Y (C % (@NP B)))
+    : (SynthRApp (L := ZRR (base:=f)) (R := arg)).denotation = (AppZRR (f:=f) (arg:=arg)).denotation :=
+      by simp
+  scoped instance AppZLR {P:Type u}[HeytingAlgebra P]{X Y A B C}
+    [f:Synth P X (rslash (lslash (@NP B) A) C)][arg:Synth P Y (C % (@NP B))]
+    : Synth P (X # Y) (lslash (@NP B) A) where
+    denotation := fun n => f.denotation _ (arg.denotation _ n) n
+    stringRep := "(AppZLR "++f.stringRep++" "++arg.stringRep++")"
+  scoped instance AppZRL {P:Type u}[HeytingAlgebra P]{X Y A B C}
+    [arg:Synth P X (C % (@NP B))][f:Synth P Y (lslash C (rslash A (@NP B)))]
+    : Synth P (X # Y) (rslash A (@NP B)) where
+    denotation := fun n => f.denotation _ (arg.denotation _ n) n
+    stringRep := "(AppZRL "++f.stringRep++" "++arg.stringRep++")"
+  scoped instance AppZLL {P:Type u}[HeytingAlgebra P]{X Y A B C}
+    [arg:Synth P X (C % (@NP B))][f:Synth P Y (lslash C (lslash (@NP B) A))]
+    : Synth P (X # Y) (rslash A (@NP B)) where
+    denotation := fun n => f.denotation _ (arg.denotation _ n) n
+    stringRep := "(AppLRL "++f.stringRep++" "++arg.stringRep++")"
+
+  -- For now we'll keep the word 'the' under wraps as well
+  scoped instance (priority := high) the_lex {P}[HeytingAlgebra P]{T}: lexicon P "the" (rslash (@NP T % @NP T) (@CN T)) where
+    denotation := fun cn x => x
+  /- Another difficulty here is that for modified CNs (those that don't just denote true), this definition discards the restriction --- e.g., 'the even number' could resolve to an 'odd number'!
+
+  One option would be to tweak CNs again to have them also reflect their predicate into the typeclass unification problem. But that will lead to problems with TC search failing for equivalent but not definitionally equal predicates (even positive vs positive even).
+
+  Another approach would be to treat 'the' as a GQ that gets its argument externally (so, type is roughtly GQ|NP, so the semantics capture sentence-combining things), then could apply the CN predicate to whatever gets bound as a sanity check.
+  -/
+
+end Anaphora
+
 
 
 
